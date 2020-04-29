dae75dabf989cd8a040bed485f27e9b27ac53157
Mike Allanson <michael.allanson@gmail.com>
2020-04-29 16:55:32 +0100

    Use lintPostcssResult straight from stylelint
    This removes my experimental / half-baked custom implementation in favour of using the function from stylelint. Stylelint doesn't expose this function in any way, so it's been copy + pasted to lintPostcssResult.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 const LazyResult = require("postcss/lib/lazy-result");
 const postcss = require("postcss");
-const get = require("lodash/get");
 
-const getOsEol = require("stylelint/lib/utils/getOsEol");
 const normalizeRuleSettings = require("stylelint/lib/normalizeRuleSettings");
-const reportUnknownRuleNames = require("stylelint/lib/reportUnknownRuleNames");
-const rulesOrder = require("stylelint/lib/rules");
-
-const allRules = require("./rules.js");
+const lintPostcssResult = require("./lintPostcssResult");
 
 const postcssProcessor = postcss();
 
@@ -40,127 +35,24 @@ async function lint(text, config) {
   };
 
   // TODO: why create a LazyResult here? Why not use postcss.process()?
-  const lazyResult = await new LazyResult(
-    postcssProcessor,
-    text,
-    postcssOptions
-  );
+  const result = await new LazyResult(postcssProcessor, text, postcssOptions);
+
+  result.stylelint = {};
 
   const normalizedConfig = normalizeAllRuleSettings(config);
 
-  return lintPostcssResult(lazyResult, normalizedConfig);
+  return lintPostcssResult({ _options: {} }, result, normalizedConfig).then(
+    () => result
+  );
 }
 
-// Run normalizeRuleSettings on all rules in the config
+// // Run normalizeRuleSettings on all rules in the config
 function normalizeAllRuleSettings(config) {
   let normalizedRules = {};
   Object.entries(config.rules).forEach(([ruleName, ruleSettings]) => {
     normalizedRules[ruleName] = normalizeRuleSettings(ruleSettings, ruleName);
   });
   return { ...config, rules: normalizedRules };
-}
-
-/**
- * @param {PostcssResult} postcssResult
- * @param {import('stylelint').StylelintConfig} config
- * @returns {Promise<any>}
- */
-function lintPostcssResult(postcssResult, config) {
-  postcssResult.stylelint = {};
-  postcssResult.stylelint.ruleSeverities = {};
-  postcssResult.stylelint.customMessages = {};
-  postcssResult.stylelint.stylelintError = false;
-  postcssResult.stylelint.quiet = config.quiet;
-
-  /** @type {string} */
-  let newline;
-  const postcssDoc = postcssResult.root;
-
-  if (postcssDoc) {
-    if (!("type" in postcssDoc)) {
-      throw new Error("Unexpected Postcss root object!");
-    }
-
-    // @ts-ignore TODO TYPES property css does not exists
-    const newlineMatch =
-      postcssDoc.source && postcssDoc.source.input.css.match(/\r?\n/);
-
-    newline = newlineMatch ? newlineMatch[0] : getOsEol();
-
-    // assignDisabledRanges(postcssDoc, postcssResult);
-  }
-
-  postcssResult.stylelint.ignoreDisables = false;
-  postcssResult.stylelint.disableWritingFix = true;
-
-  const postcssRoots = /** @type {import('postcss').Root[]} */ (postcssDoc &&
-  postcssDoc.constructor.name === "Document"
-    ? postcssDoc.nodes
-    : [postcssDoc]);
-
-  // Promises for the rules. Although the rule code runs synchronously now,
-  // the use of Promises makes it compatible with the possibility of async
-  // rules down the line.
-  /** @type {Array<Promise<any>>} */
-  const performRules = [];
-
-  const rules = config.rules
-    ? Object.keys(config.rules).sort(
-        (a, b) =>
-          Object.keys(rulesOrder).indexOf(a) -
-          Object.keys(rulesOrder).indexOf(b)
-      )
-    : [];
-
-  rules.forEach((ruleName) => {
-    const ruleFunction =
-      allRules[ruleName] || get(config, ["pluginFunction", ruleName]);
-
-    if (ruleFunction === undefined) {
-      performRules.push(
-        Promise.all(
-          postcssRoots.map((postcssRoot) =>
-            reportUnknownRuleNames(ruleName, postcssRoot, postcssResult)
-          )
-        )
-      );
-
-      return;
-    }
-
-    const ruleSettings = get(config, ["rules", ruleName]);
-
-    if (ruleSettings === null || ruleSettings[0] === null) {
-      return;
-    }
-
-    const primaryOption = ruleSettings[0];
-    const secondaryOptions = ruleSettings[1];
-
-    // Log the rule's severity in the PostCSS result
-    const defaultSeverity = config.defaultSeverity || "error";
-
-    postcssResult.stylelint.ruleSeverities[ruleName] = get(secondaryOptions, [
-      "severity",
-      defaultSeverity,
-    ]);
-    postcssResult.stylelint.customMessages[ruleName] = get(secondaryOptions, [
-      "message",
-    ]);
-
-    performRules.push(
-      Promise.all(
-        postcssRoots.map((postcssRoot) =>
-          ruleFunction(primaryOption, secondaryOptions, {
-            fix: false, // TODO: fix fix not fixing
-            newline,
-          })(postcssRoot, postcssResult)
-        )
-      )
-    );
-  });
-
-  return Promise.all(performRules).then(() => postcssResult);
 }
 
 module.exports = lint;
